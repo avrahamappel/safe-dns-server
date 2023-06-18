5dad5e597ec5624d5123ea0040cb9c1062e76dff
Avraham Appel <aappel248@gmail.com>
2023-06-18 15:09:14 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "A DNS server container that proxies to OpenDNS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+
+      upstreamDns = [
+        "208.67.222.222"
+        "208.67.220.220"
+      ];
+
+      cnames = {
+        # Google
+        "www.google.com" = "forcesafesearch.google.com";
+        "www.google.ca" = "forcesafesearch.google.com";
+
+        # YouTube
+        "www.youtube.com" = "restrict.youtube.com";
+        "m.youtube.com" = "restrict.youtube.com";
+        "youtubei.googleapis.com" = "restrict.youtube.com";
+        "youtube.googleapis.com" = "restrict.youtube.com";
+        "www.youtube-nocookie.com" = "restrict.youtube.com";
+
+        # DuckDuckGo
+        "duckduckgo.com" = "safe.duckduckgo.com";
+      };
+
+      lines = strs: builtins.concatStringsSep "\n" strs;
+
+      config = pkgs.writeText "dnsmasq.conf" ''
+        ${lines (map (s: "server ${s}") upstreamDns)}
+        ${lines (builtins.attrValues (builtins.mapAttrs (from: to: "cname ${from} ${to}") cnames))}
+      '';
+    in
+    {
+      packages.default = pkgs.dockerTools.buildImage {
+        name = "home-dns";
+
+        copyToRoot = pkgs.buildEnv {
+          name = "image-root";
+          paths = [ pkgs.dnsmasq ];
+          pathsToLink = [ "/bin" ];
+        };
+
+        config = {
+          Cmd = [ "dnsmasq" "-C" config ];
+        };
+      };
+    });
+}
